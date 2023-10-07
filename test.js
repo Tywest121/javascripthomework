2232b3fef7a592161e70fbe5e51fc791587c1cf9
tywest121 <tyresewest25@gmail.com>
2023-10-07 16:05:04 -0400

    Joke

---
+++ test.js
@@ -0,0 +1,12 @@
+// the first alert
+alert("Hello,World!");
+
+// the second alert
+alert("Hello,World!");
+
+//move the image from top to bottom
+//make the image invisble
+//make the image visble
+
+
+
